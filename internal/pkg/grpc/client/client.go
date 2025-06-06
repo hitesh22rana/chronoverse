84f3fe7a641005ba6f45cba20073a5c382b6f33e
hitesh22rana <hitesh22rana@gmail.com>
2025-06-06 19:43:39 +0900

    fix: grpc client retry on certain error codes

--- internal/pkg/grpc/client/client.go
+++ internal/pkg/grpc/client/client.go
@@ -28,6 +28,8 @@ type RetryConfig struct {
 	BackoffExponential time.Duration
 	// RetryableCodes is a list of status codes that are retryable.
 	RetryableCodes []codes.Code
+	// PerRetryTimeout is the timeout for each retry attempt (handles case for context.DeadlineExceeded and context.Canceled).
+	PerRetryTimeout time.Duration
 }
 
 // DefaultRetryConfig returns a default retry configuration.
@@ -38,51 +40,50 @@ func DefaultRetryConfig() *RetryConfig {
 		RetryableCodes: []codes.Code{
 			codes.Unavailable,
 			codes.ResourceExhausted,
-			codes.DeadlineExceeded,
 			codes.Aborted,
 		},
+		PerRetryTimeout: 2 * time.Second,
 	}
 }
 
 // NewClient creates a new gRPC client connection with retry and tracing support.
 func NewClient(svcCfg ServiceConfig, retryCfg *RetryConfig) (*grpc.ClientConn, error) {
+	var opts []grpc.DialOption
+
 	// Set default retry config if not provided
 	if retryCfg == nil {
 		retryCfg = DefaultRetryConfig()
 	}
 
-	// Load credentials
-	var creds credentials.TransportCredentials
-	var err error
-	if svcCfg.Secure {
-		if creds, err = credentials.NewClientTLSFromFile(svcCfg.CertFile, ""); err != nil {
-			return nil, status.Errorf(codes.Internal, "failed to load TLS credentials: %v", err)
-		}
-	} else {
-		creds = insecure.NewCredentials()
-	}
-
 	// Configure retry options
 	retryOpts := []retry.CallOption{
-		retry.WithMax(retryCfg.MaxRetries),
-		retry.WithBackoff(
-			retry.BackoffExponential(retryCfg.BackoffExponential),
-		),
 		retry.WithCodes(retryCfg.RetryableCodes...),
+		retry.WithMax(retryCfg.MaxRetries),
+		retry.WithBackoff(retry.BackoffExponential(retryCfg.BackoffExponential)),
+		retry.WithPerRetryTimeout(retryCfg.PerRetryTimeout),
 	}
 
-	// Setup connection options
-	dialOpts := []grpc.DialOption{
-		grpc.WithTransportCredentials(creds),
-		grpc.WithChainUnaryInterceptor(
-			retry.UnaryClientInterceptor(retryOpts...),
-		),
+	opts = append(
+		opts,
+		grpc.WithUnaryInterceptor(retry.UnaryClientInterceptor(retryOpts...)),
+		grpc.WithStreamInterceptor(retry.StreamClientInterceptor(retryOpts...)),
+	)
+
+	// Load credentials
+	if svcCfg.Secure {
+		creds, err := credentials.NewClientTLSFromFile(svcCfg.CertFile, "")
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to load TLS credentials: %v", err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else {
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	}
 
 	// Connect to the service
 	conn, err := grpc.NewClient(
 		fmt.Sprintf("%s:%d", svcCfg.Host, svcCfg.Port),
-		dialOpts...,
+		opts...,
 	)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "failed to connect to gRPC server: %v", err)
